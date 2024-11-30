03cfe69734eeabc85bde74fbbc95c515f3311046
Eric Lesniewski <elesniewski@comcast.net>
2024-11-30 10:14:39 -0500

    End of Section 14

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,3 +1,4 @@
+import { LikesService } from './likes.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject, signal } from '@angular/core';
 import { User } from '../_models/user';
@@ -9,6 +10,7 @@ import { environment } from '../../environments/environment';
 })
 export class AccountService {
   private http = inject(HttpClient);
+  private likeService = inject(LikesService);
   baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
 
@@ -35,7 +37,8 @@ export class AccountService {
 
   setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user);
+    this.currentUser.set(user);
+    this.likeService.getLikeIds();
   }
 
   logout() {
